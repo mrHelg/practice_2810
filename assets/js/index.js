e3150b1e45e77d9325201d925711c71569e8a96b
Som&soft <som8soft@gmail.com>
2021-11-06 20:16:48 +0200

    Final version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,48 @@
-'use strict'
+'use strict';
+
+const buttons = document.getElementsByClassName('btn');
+const itemSection = document.getElementById('item-list');
+const items = document.getElementsByClassName('item');
+let activeBtn = null;
+
+for (const button of buttons) {
+  button.textContent = button.dataset.flexDirection;
+  button.addEventListener('click', flexHandler);
+  if (button.dataset.currentItem === 'true') {
+    setActiveButton(button);
+    setFlexDirection(button);
+    activeBtn = button;
+  } else {
+    setUnactiveButton(button);
+  }
+}
+
+function setActiveButton(button) {
+  button.style.background = 'cornflowerblue';
+  button.style.color = 'white';
+}
+
+function setUnactiveButton(button) {
+  button.style.background = 'white';
+  button.style.color = 'black';
+}
+
+function setFlexDirection({dataset: {flexDirection}}) {
+  itemSection.style.flexDirection = flexDirection;
+  for (const item of items) {
+    if (flexDirection.includes('COLUMN')) {
+      item.style.width = '100%';
+      item.style.height = 'unset';
+    } else {
+      item.style.width = 'unset';
+      item.style.height = '100%';
+    }
+  }
+}
+
+function flexHandler({ target: currentBtn }) {
+  setUnactiveButton(activeBtn);
+  setActiveButton(currentBtn);
+  setFlexDirection(currentBtn);
+  activeBtn = currentBtn;
+}
